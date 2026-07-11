c3e9f9bdf0351eb54a9868d8439a62da1b842a2b
Sukhman3125 <sukhman3125@gmail.com>
2026-07-11 19:35:26 +0530

    project setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config();
+console.log("MONGODB_URI =", process.env.MONGODB_URI);
+
+connectDB()
+.then(()=>{
+    const port = process.env.PORT || 8000;
+    app.listen(port, ()=>{
+        console.log(`server running at port: ${port}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed ", err);
+});
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";    // filesystem
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_CLOUD_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+        fs.unlinkSync(localFilePath);
+        return response;
+    }catch(err){
+        fs.unlinkSync(localFilePath); // remove from local if failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary};
