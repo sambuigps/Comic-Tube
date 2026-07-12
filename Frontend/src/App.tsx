5eaa1a2b30a4d7194263e293684c213a5d292664
mars985 <clasherprofile.3@gmail.com>
2026-07-12 23:01:33 +0530

    frontend setup

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='w-screen h-screen text-4xl'>
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
